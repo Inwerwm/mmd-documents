e7e83e5c8ee22f22a4062be4c85fb32298976ee5
inwerwm <saclainvervm@gmail.com>
2021-06-14 18:04:11 +0900

    ローカルでhtml生成を試せるようにした

---
+++ script/Docker-Run.ps1
@@ -0,0 +1,1 @@
+docker run -it -v "/c/MMD/_Doc/mmd-documents:/srv/src" -p 4000:4000 --name mmd-documents github-pages
---
+++ script/Docker-Start.ps1
@@ -0,0 +1,3 @@
+Write-Output "Address: http://127.0.0.1:4000/"
+Write-Output "終了時は停止すること!"
+docker start mmd-documents
