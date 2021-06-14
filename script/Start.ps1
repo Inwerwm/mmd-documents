5d3b349fbfa4d51981782001462799d4322fa7e6
inwerwm <saclainvervm@gmail.com>
2021-06-14 18:32:46 +0900

    文章の変更

--- script/Start.ps1
+++ script/Start.ps1
@@ -1,6 +1,4 @@
-Write-Output "コンテナを起動"
+Write-Output "コンテナの起動を開始"
 docker start mmd-documents
 Write-Output "完了"
-Write-Output ""
 Write-Output "Address: http://127.0.0.1:4000/"
-Write-Output "終了時は停止すること!"
--- script/Stop.ps1
+++ script/Stop.ps1
@@ -1,3 +1,3 @@
-Write-Output "コンテナを終了"
+Write-Output "コンテナの停止を開始"
 docker stop mmd-documents
 Write-Output "完了"
